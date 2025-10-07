2bc34bb702b6679da9b2e76d056d7f0b2bc4d6e8
riyasalvi-git <riyasalvi@protonmail.com>
2025-10-07 15:57:13 +0530

    Optimize README layout, add js script, add comments for better understanding

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
-- initalize the function to get computer choice.
-- since don't know arrays yet, use Math.random() to get the computer choice.
-- get 3 random numbers from math random and associate each number with the 3 choices we have using switch statements.
--
+## Structure:
+- Initialize score and round variables.
+- Initalize the function to get computer choice. Use Math.random() to get the computer choice. Get 3 random numbers from math random and associate each number with the 3 choices we have using switch statements.
+- Play the game with the input of rounds. If rounds are less than 5 then get both player choices and play the round else print winner.
+- Evaluate the choices with switch case, increment the score and print round winner.
+- Increment rounds to suggest completion of a round.
+- Taking the incremented rounds as input for playGame play the function again.
+- Used if else to create a loop and put the game function again at the end of the round func to reevaluate the rounds and keep playing.
+
+## Mistakes/Tips:
+- Remember to use the + operator to concatenate the string.
+- Try looking for syntax errors in different parts of the line of code.
+- Use true in switch expressions in case of multiple expressions.
+- If the case in switch case is long or has conditional operators then use brackets around it.
+
---
+++ index.js
@@ -0,0 +1,84 @@
+		// declare initial variables
+		let humanScore = 0;
+		let computerScore = 0;
+		let round = 0;
+
+		playGame(round);
+
+		//  evaluate the number of rounds with if/else to see if have to play or just print the winner
+		function playGame(round) {
+			if (round < 5) {   				//Play until 5 rounds are complete.
+				computerChoice = getComputerChoice();
+				let getHumanChoice = prompt("What do you choose?");
+				humanChoice = getHumanChoice.toLowerCase();
+				playRound(humanChoice, computerChoice);
+			}
+			else {     					// Print winner
+				if (computerScore > humanScore) {
+					console.log("Computer wins! Computer score is " + computerScore + " while your score is " + humanScore + ".");
+				}
+				else if (humanScore > computerScore) {
+					console.log("You win! Your score is " + humanScore + " while computer score is " + computerScore + ".");
+				}
+				else {
+					console.log("Opps! Its a tie!.")
+				}
+			}
+		}
+
+		//get computer choice
+		function getComputerChoice() {
+			let choice;
+ 			let num = Math.floor(Math.random() * 3);
+			switch (num) {
+				case 0:
+					choice = "rock";
+					break;
+				case 1:
+					choice = "paper";
+					break;
+				case 2:
+					choice = "scissors";
+					break;
+			}
+			return choice;
+		}
+
+		// play a single round of the game and increment the number of rounds. 
+		// input the rounds in playGame to play more rounds if needed. While printing the winner
+		function playRound(humanChoice, computerChoice) {
+			let message;
+			switch (true) {
+				case (humanChoice === "rock" && computerChoice === "scissors"):
+					++humanScore;
+					message = "You win! Rock beats scissors!";
+					break;
+				case (humanChoice === "rock" && computerChoice === "paper"):
+					++computerScore;
+					message = "You lose! Paper beats rock!";
+					break;
+				case (humanChoice === "paper" && computerChoice === "scissors"):
+					++computerScore;
+					message = "You lose! Scissors beats paper!";
+					break;
+				case (humanChoice === "paper" && computerChoice === "rock"):
+					++humanScore;
+					message = "You win! Paper beats rock!";
+					break;
+				case (humanChoice === "scissors" && computerChoice === "rock"):
+					++computerScore;
+					message = "You lose! Rock beats scissors!";
+					break;
+				case (humanChoice === "scissors" && computerChoice === "paper"):
+					++humanScore;
+					message = "You win! Scissors beats paper!";
+					break;
+				case (humanChoice === computerChoice):
+					message = "Oops! Its a tie!";
+					break;
+			}
+
+			console.log("You chose " + humanChoice.toUpperCase() + " and computer chose " + computerChoice.toUpperCase() + "." + "\n" + message + " Your score is " + humanScore + " and computer score is " + computerScore);
+			++round;
+			playGame(round);    			// evaluate the number of rounds according to the previous increment to play more.
+		}
